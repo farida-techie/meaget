0b206474b09cc94b0140f3de0e3e51e01010cd60
shekh <fairdabhairakdar@gmail.com>
2020-11-26 12:28:57 +0530

    Toast Mess

---
+++ meaget/src/main/java/com/malkinfo/meaget/MeasgT.kt
@@ -0,0 +1,10 @@
+package com.malkinfo.meaget
+
+import android.content.Context
+import android.widget.Toast
+
+class MeasgT {
+    fun meseT(c:Context,mss :String){
+        Toast.makeText(c,mss,Toast.LENGTH_SHORT).show()
+    }
+}
